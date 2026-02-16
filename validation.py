7a981e2cc3a68c1e0a080f11ff87df622813ade9
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-16 10:27:11 +0300

    Provided logic that states 'Task title cannot be empty' if the title is empty or just whitesace

---
+++ validation.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+def validate_task_title(title):
+    if not title or not title.strip():
+        print("Error: Task title cannot be empty.")
+        return False
+    
+def validate_task_description(description):
+    None    
+    
+def validate_due_date(due_date):
+    None
