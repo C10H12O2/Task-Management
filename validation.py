d8bff8a7313f6667057d73371bfbb1440fbd56ab
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-16 10:31:45 +0300

    Provided logic that checks the minimum title length, if it doesnt meet the required character length an error message will be displayed

--- validation.py
+++ validation.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 
+
 def validate_task_title(title):
     if not title or not title.strip():
         print("Error: Task title cannot be empty.")
         return False
-    
+
+    if len(title.strip()) < 3:
+        print("Error: Task title must be at least 3 characters long.")
+        return False
+
+    return True
+
+
 def validate_task_description(description):
-    None    
-    
+    None
+
+
 def validate_due_date(due_date):
     None
