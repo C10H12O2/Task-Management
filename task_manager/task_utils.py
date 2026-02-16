d498e19a2e06ca27c376250f7188e6b9a73beff6
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-02-16 13:24:58 +0300

    implemented an add_task function with due date, description adn title validation, created a task dictionary, and add to list logic that of which then prints 'Task added successfully'

---
+++ task_manager/task_utils.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+# Import validation functions
+from task_manager.validation import (validate_task_title, validate_task_description, validate_due_date)
+
+# Define tasks list
+tasks = []
+
+# Implement add_task function
+def add_task(title, description, due_date):
+    if not validate_task_title(title):
+        return
+    if not validate_task_description(description):
+        return
+    if not validate_due_date(due_date):
+        return
+    
+    task = {
+        "title": title.strip(),
+        "description": description.strip() if description else "",
+        "due_date": due_date.strip(),
+        "completed": False
+    }
+    tasks.append(task)
+    print("Task has been added successfully!")
+    
+# Implement mark_task_as_complete function
+def mark_task_as_complete(index, tasks=tasks):
+    if index < 0 or index >= len(tasks):
+        print("Error: Invalid task number.")
+        return
+    
+    if tasks[index]["completed"]:
+        print(f"Task '{tasks[index]['title']}' is already marked as complete.")
+        return
+    
+    tasks[index]["completed"] = True
+    print("Task marked as complete!")
+    
+# Implement view_pending_tasks function
+def view_pending_tasks(tasks=tasks):
+    None
+
+# Implement calculate_progress function
+def calculate_progress(tasks=tasks):
+    None
+    return progress
